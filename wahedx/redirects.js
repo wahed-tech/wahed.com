530c379a3b5bd8d4bf10ff55f62a584e2de3dd19
Rifkie Drajat <rifkiedr@gmail.com>
2023-12-11 14:36:15 +0700

    WahedX Redirects

---
+++ wahedx/redirects.js
@@ -0,0 +1,22 @@
+// geotargetly_continent_name(); //gets visitor continent name
+// geotargetly_continent_code(); //gets visitor continent code
+// geotargetly_country_name(); //gets visitor country name
+// geotargetly_country_code(); //gets visitor country code
+// geotargetly_region_name(); //gets visitor Region/State or state name
+// geotargetly_region_code(); //gets visitor Region/State or state code
+// geotargetly_city_name(); // gets visitor city name
+// geotargetly_lat(); // gets visitor latitude
+// geotargetly_lng(); // gets visitor longitude 
+// geotargetly_ip(); // gets visitor IP address
+// geotargetly_currency_code(); // gets visitor currency code
+// geotargetly_currency_symbol(); // gets visitor currency symbol
+// geotargetly_calling_code(); // gets visitor calling code
+// geotargetly_loaded(); // gets called when data is available
+
+// We are using Geotargetly Location
+
+function redirectToExternalURL(url) {
+    window.location.href = url;
+}
+
+redirectToExternalURL('https://www.wahed.com/uk/ventures');
